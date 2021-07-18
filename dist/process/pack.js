1d494a9e7aee7a432a06254d16ea4019ca70a182
kangdongmandoo <kangdongmandoo@gmail.com>
2021-07-18 13:05:00 +0900

    Create pack.js

---
+++ dist/process/pack.js
@@ -0,0 +1,26 @@
+"use strict";
+exports.__esModule = true;
+exports.process = exports.ReadData = void 0;
+var sortObject_1 = require("../sortObject");
+function ReadData(data) {
+    var score = {};
+    for (var index = 0; index < data.length; index++) {
+        score[data.charAt(index)] = score[data.charAt(index)] + 1 || 1;
+    }
+    return score;
+}
+exports.ReadData = ReadData;
+function process(data, map) {
+    var r = "";
+    var m = Object.keys(map).reverse();
+    for (var index = 0; index < data.length; index++) {
+        r = "" + r + m.indexOf(data[index]) + " ";
+    }
+    return r;
+}
+exports.process = process;
+function pack(data) {
+    var m = sortObject_1["default"](ReadData(data));
+    return { v: process(data, m), m: m };
+}
+exports["default"] = pack;
