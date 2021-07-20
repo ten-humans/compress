e1da4bcaad678e06c71a35e6e94ec11274160676
kangdongmandoo <kangdongmandoo@gmail.com>
2021-07-20 19:59:42 +0900

    Create pack.d.ts

---
+++ dist/process/pack.d.ts
@@ -0,0 +1,4 @@
+import { packType, ScoreType } from "../interface/process";
+export declare function ReadData(data: string): ScoreType;
+export declare function process(data: string, map: object): string;
+export default function pack(data: string): packType;
